0293e39f26faeda1fc2f218393b5ec4a5d935d70
Peter Goes <petergoes@gmail.com>
2020-10-19 09:00:41 +0200

    initial commit

---
+++ pages/_slug.vue
@@ -0,0 +1,15 @@
+<template>
+  <main>
+    <h1>{{ doc.title }}</h1>
+    <nuxt-content :document="doc" />
+  </main>
+</template>
+
+<script>
+export default {
+  async asyncData ({ $content, params }) {
+    const doc = await $content(params.slug).fetch()
+    return { doc }
+  }
+}
+</script>
